365a35d0f1a77bb038969d22b386c133e64f49c2
Gunslinger489 <lucasg3.gdh@gmail.com>
2021-09-19 21:51:18 -0300

    Calcula a média de 2 números reais

---
+++ exe007.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Digite a primeira nota do aluno: "))
+nota2 = float(input("Digite a segunda nota do aluno: "))
+
+mediaNota = float((nota1 + nota2) / 2)
+
+print(f'A média entre {nota1:.2f} e {nota2:.2f} é igual a {mediaNota:.2f}')
