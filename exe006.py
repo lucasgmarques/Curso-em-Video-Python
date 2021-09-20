e120b8af89bdc2635c9b01cb0d84740d4004fa8a
Gunslinger489 <lucasg3.gdh@gmail.com>
2021-09-19 22:46:44 -0300

    Verifica o número posterior e anterior

---
+++ exe006.py
@@ -0,0 +1,7 @@
+num = int(input(f'Digite um número: '))
+'''
+numAnt = num - 1
+numPos = num + 1
+'''
+print(f'Você digitou o número {num}.')
+print(f'O número antecessor é {num - 1} e o número posterior é {num + 1}.')
