b65176e48a05e402ce30a903003820633099b1a9
Lucas Marques <lucasg3.gdh@gmail.com>
2023-05-02 16:35:16 -0300

    Transformando Celsius para Fahrenheit

---
+++ exe014.py
@@ -0,0 +1,5 @@
+tempCelsius = float(input("Digite a temperatura em ° Celsius: "))
+
+tempFahrenheit = (tempCelsius * 1.8) + 32
+
+print(f'A temperatura de {tempCelsius:.1f}°C corresponde a {tempFahrenheit:.1f}°F')
