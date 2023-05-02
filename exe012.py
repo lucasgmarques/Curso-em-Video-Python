a4660ac6ac6f09e3426b266fcf9df788d94e6fb7
Lucas Marques <lucasg3.gdh@gmail.com>
2023-05-02 15:04:34 -0300

    Added new variable

--- exe012.py
+++ exe012.py
@@ -2,6 +2,7 @@ preco_produto = float(input("Qual o preço do produto? R$"))
 percentual_desconto = float(input("Qual a taxa de desconto? "))
 
 # CALCULO DO DESCONTO
-desconto_prod = preco_produto - (preco_produto * percentual_desconto/100)
+desconto_prod = (preco_produto * percentual_desconto/100)
+novo_preco = preco_produto - desconto_prod
 
-print(f'O produto que custava R${preco_produto:.2f}, na promoção com desconto de {percentual_desconto:.0f}% vai custar R${desconto_prod:.2f}')
+print(f'O produto que custava R${preco_produto:.2f}, na promoção com desconto de {percentual_desconto:.0f}% vai custar R${novo_preco:.2f}')
